8954fd551d02e1466dc8f8f7f1b3dcc96fb91ae8
khushi1997n <40892595+khushi1997n@users.noreply.github.com>
2021-02-17 13:53:13 +0530

    Delete scripts directory

--- scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-isExistApp=`pgrep apache2`
-if [[ -n  $isExistApp ]]; then
-   systemctl restart apache2
-fi
-isExistApp=`pgrep mysql`
-if [[ -n  $isExistApp ]]; then
-    systemctl restart mysql
-fi
